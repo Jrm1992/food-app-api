aefc1dacfa041c67d9b6957ce94c60788a4d3dca
Jrm1992 <jose_roberto_6@msn.com>
2023-11-03 10:55:32 -0300

    feat: Product DTO validation

--- src/main/java/com/foodapp/foodapp/domain/product/ProductRequestDTO.java
+++ src/main/java/com/foodapp/foodapp/domain/product/ProductRequestDTO.java
@@ -1,9 +1,17 @@
 package com.foodapp.foodapp.domain.product;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record ProductRequestDTO(
-    String name,
-    Integer price,
-    String description
+        @NotBlank
+        String name,
+
+        @NotNull
+        Integer price,
+
+        @NotNull
+        String description
 
 ) {
 }
