571fa9fc4446e39edee41f44235b7caa6847fe59
Jrm1992 <jose_roberto_6@msn.com>
2023-11-02 21:28:06 -0300

    feat: Product model

---
+++ src/main/java/com/foodapp/foodapp/domain/product/Product.java
@@ -0,0 +1,31 @@
+package com.foodapp.foodapp.domain.product;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "product")
+@Entity(name = "product")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Integer price;
+
+    private String description;
+
+    public Product(ProductRequestDTO product) {
+        this.name = product.getName();
+        this.price = product.getPrice();
+        this.description = product.getDescription();
+    }
+}
