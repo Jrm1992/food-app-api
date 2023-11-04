2b7fbf09a1c58e62d8625aa420393571b9a822cc
Jrm1992 <jose_roberto_6@msn.com>
2023-11-03 21:42:58 -0300

    fix: disable SecurityAutoConfiguration

--- src/main/java/com/foodapp/foodapp/FoodAppApplication.java
+++ src/main/java/com/foodapp/foodapp/FoodAppApplication.java
@@ -2,8 +2,9 @@ package com.foodapp.foodapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class FoodAppApplication {
 
 	public static void main(String[] args) {
