7a330c1da0ba37849648c622ddf2668596732814
Jrm1992 <jose_roberto_6@msn.com>
2023-11-05 11:38:03 -0300

    feat: TokenService

---
+++ src/main/java/com/foodapp/foodapp/services/TokenService.java
@@ -0,0 +1,44 @@
+package com.foodapp.foodapp.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.foodapp.foodapp.domain.user.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("FoodApp")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                    .sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Error while generating token", e);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("FoodApp")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            return "";
+        }
+    }
+}
